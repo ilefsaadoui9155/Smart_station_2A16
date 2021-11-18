956d1bb8d628e846d52a565aa8412eb004267a72
yassinedhib <yassine.dhib@esprit.tn>
2021-11-18 17:33:20 +0100

    forth commit

--- gestion-de-reparation/reparation.cpp
+++ gestion-de-reparation/reparation.cpp
@@ -93,12 +93,12 @@ bool reparation::modifierj(int refrep,QString type,QString matricule,int nombre)
 
 }
 
-bool reparation :: supprimer(int id)
+bool reparation :: supprimer(int refrep)
 {
    // QString ident1= QString ::number(refrep);
     QSqlQuery query;
-    query.prepare("DELETE from table1 where refrep = :id ");
-    query.bindValue(":id",id);
+    query.prepare("DELETE from table1 where refrep = :refrep ");
+    query.bindValue(":refrep",refrep);
     return    query.exec();
 
 }
@@ -118,5 +118,61 @@ QSqlQueryModel* reparation::rechercher (const QString &aux)
     return model;
 
 }
+
+void  Employe::genererPDF(){
+//QDateTime datecreation = date.currentDateTime();
+              //QString afficheDC = "Date de Creation PDF : " + datecreation.toString() ;
+                     QPdfWriter pdf("D:/ProjetCpp/gestion-de-reparation/liste.pdf");
+                     QPainter painter(&pdf);
+                    int i = 4000;
+                         painter.setPen(Qt::red);
+                         painter.setFont(QFont("Arial", 30));
+                         painter.drawText(1100,1200,"Liste Des reparation");
+                         painter.setPen(Qt::black);
+                         painter.setFont(QFont("Arial", 15));
+                        // painter.drawText(1100,2000,afficheDC);
+                         painter.drawRect(100,100,7300,2600);
+
+                         painter.drawRect(0,3000,9600,500);
+                         painter.setFont(QFont("Arial", 9));
+                         painter.drawText(200,3300,"refrance");
+                         painter.drawText(1300,3300,"matricule");
+                         painter.drawText(2100,3300,"type");
+                         painter.drawText(2100,3300,"nombre");
+
+                         QSqlQuery query;
+                         query.prepare("select * from table1");
+                         query.exec();
+                         while (query.next())
+                         {
+                             painter.drawText(200,i,query.value(0).toString());
+                             painter.drawText(1300,i,query.value(1).toString());
+                             painter.drawText(2200,i,query.value(2).toString());
+                             painter.drawText(3200,i,query.value(4).toString());
+
+                            i = i + 500;
+                         }
+
+
+      int reponse = QMessageBox::Yes;
+          if (reponse == QMessageBox::Yes)
+          {
+              QSystemTrayIcon *notifyIcon = new QSystemTrayIcon;
+              notifyIcon->show();
+             notifyIcon->setIcon(QIcon("icone.png"));
+
+              notifyIcon->showMessage("GESTION DES  EMPLYOES ","Liste employée pret dans PDF",QSystemTrayIcon::Information,15000);
+
+              painter.end();
+          }
+          if (reponse == QMessageBox::No)
+          {
+               painter.end();
+          }
+}
+
+
+
+
 
 
